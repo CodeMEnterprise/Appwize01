f2a207303917352495334db219ec03042a4f265b
Joshua Wiegman <18077jw@sierra1724.local>
2019-03-25 12:05:37 +1300

    Initial Commit

---
+++ AppwizeTests/AppwizeTests.swift
@@ -0,0 +1,36 @@
+//
+//  AppwizeTests.swift
+//  AppwizeTests
+//
+//  Created by Joshua Wiegman on 25/03/19.
+//  Copyright © 2019 Joshua Wiegman. All rights reserved.
+//
+
+import XCTest
+@testable import Appwize
+
+class AppwizeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
